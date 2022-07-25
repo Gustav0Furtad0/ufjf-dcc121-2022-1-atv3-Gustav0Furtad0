f6ff4d8a7f0105a868444ca53e27d844c5e5a25e
Gustav0Furtad0 <gufurtado02@gmail.com>
2022-07-25 14:37:57 -0300

    comentarios addlista

--- addlista.js
+++ addlista.js
@@ -1,16 +1,21 @@
+//buscando endereço dos elementos
 const btsend = document.querySelector("#btsend");
 const inname = document.querySelector("#name");
 const inage = document.querySelector("#age");
 
-btsend.addEventListener('click', (e) => {
+btsend.addEventListener('click', e => {
+    //bloqueando action do botão
     e.preventDefault();
     let name = inname.value;
     let age = Number(inage.value);
     let level;
+
+    //reset do formulário
     inname.value = '';
     inage.value = '';
     inname.focus();
 
+    //teste de idade para adicionar usuário
     if (age < 18)
         level = "listajunior"
     else if (age <= 40)
@@ -18,8 +23,9 @@ btsend.addEventListener('click', (e) => {
     else
         level = "listasenior"
 
+    //adicionando usuário na lista correspondente
     let addlist = document.querySelector(`#${level}`);
     let li = `<li>Nome: ${name}; Idade: ${age};`;
     addlist.innerHTML += li;
-    
+    //alert("DEV adicionado");
 });
