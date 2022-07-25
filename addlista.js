41cf6319dd1873f525f400a28a8426e9e3bb98f1
Gustav0Furtad0 <gufurtado02@gmail.com>
2022-07-25 12:24:58 -0300

    melhorias1

---
+++ addlista.js
@@ -0,0 +1,22 @@
+const btsend = document.querySelector("#btsend");
+const inname = document.querySelector("#name");
+const inage = document.querySelector("#age");
+
+
+btsend.addEventListener('click', (e) => {
+    e.preventDefault();
+    let name = inname.value;
+    let age = Number(inage.value);
+    let level;
+
+    if (age < 18)
+        level = "listajunior"
+    else if (age <= 40)
+        level = "listamaster"
+    else
+        level = "listasenior"
+
+    let addlist = document.querySelector(`#${level}`);
+    let li = `<li>Nome: ${name}; Idade: ${age};`;
+    addlist.innerHTML += li;
+}); 
